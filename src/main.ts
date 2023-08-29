e76b0236d78a3ecab1e8968e6876b0a7c28e0b79
Alejandra Ayala <onlyourfeeling@hotmail.com>
2023-08-29 16:29:05 -0400

    configuracion swagger inicial

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle(`API Capital cervecera`)
+    .setDescription(`Contiene los servicios del sistema capital cervecera.`)
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
