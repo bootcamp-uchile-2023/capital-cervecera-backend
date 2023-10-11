2444e532e4466ea53ccdadd36e368a5e4799a7d2
Alejandra Ayala <onlyourfeeling@hotmail.com>
2023-10-11 16:20:07 -0300

    subir novadedes carrito y usuario

--- src/carrito-de-compra/dto/carrito-de-compra.dto.ts
+++ src/carrito-de-compra/dto/carrito-de-compra.dto.ts
@@ -1,6 +1,28 @@
-export class CarritoCompraDto {
-  readonly productos: string;
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsNumber } from 'class-validator';
+import {CarritoDeCompraDto} from '../carritoDeComprar.Dto';
+
+
+export class carritoDeCompraDto {
+  @ApiProperty()
+  @IsString({ message: 'el atributo debe ser un string '})
+  readonly producto: string;
+
+  @ApiProperty()
+  @IsString({}, {message: 'el atributo debe ser un string'})
   readonly promociones: string;
+
+  @ApiProperty()
+  @IsNumber({}, {message: 'el atributo debe ser un Number ' })
   readonly subtotal: number;
+
+
+  @ApiProperty()
+  @IsNumber({ message: 'el atributo debe ser un number ' })
   readonly total: number;
+
+  @ApiProperty()
+  readonly producto: carritoDeCompraDto[];
 }
+
+export default CarritoDeCompraDto;
--- src/usuario/dto/usuario.dto.ts
+++ src/usuario/dto/usuario.dto.ts
@@ -1,7 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsNumber } from 'class-validator';
+import {UsuarioDto} from '../../usuario.Dto';
+ 
 export class UsuarioDto {
+  @ApiProperty()
+  @IsString({ message: 'el atributo debe ser un string '})
   readonly id: string;
+
+  @ApiProperty()
+  @IsNumber({}, {message: 'el atributo debe ser un number'})
   readonly username: string;
+
+  @ApiProperty()
+  @IsNumber({}, {message: 'el atributo debe ser un Number ' })
   readonly email: string;
-  readonly password: number;
-  //carrito
+
+  @ApiProperty()
+  @IsString({ message: 'el atributo debe ser un string ' })
+  readonly password: string;
+
+ 
+  @ApiProperty()
+  readonly id: UsuarioDto[];
 }
+
+export default UsuarioDto;
