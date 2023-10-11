0f8f0f9bf2d98a7815a5e5c0b51eb7715d20bf1d
Alejandra Ayala <onlyourfeeling@hotmail.com>
2023-10-11 17:33:40 -0300

    subir carrito

--- src/carrito-de-compra/dto/carrito-de-compra.dto.ts
+++ src/carrito-de-compra/dto/carrito-de-compra.dto.ts
@@ -1,6 +1,52 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsNumber } from 'class-validator';
+
 export class CarritoCompraDto {
+  @ApiProperty()
+  @IsNumber({}, { message: 'El atributo debe ser un número' })
+  readonly venta_id: number;
+
+  @ApiProperty()
+  @IsNumber({}, { message: 'El atributo debe ser un número' })
+  readonly carrito: number;
+
+  @ApiProperty()
+  @IsString({ message: 'El atributo debe ser una cadena de texto' })
   readonly productos: string;
+
+  @ApiProperty()
+  @IsString({ message: 'El atributo debe ser una cadena de texto' })
   readonly promociones: string;
+
+  @ApiProperty()
+  @IsNumber({}, { message: 'El atributo debe ser un número' })
   readonly subtotal: number;
+
+  @ApiProperty()
+  @IsNumber({}, { message: 'El atributo debe ser un número' })
   readonly total: number;
+
+  @ApiProperty()
+  @IsString({ message: 'El atributo debe ser una cadena de texto' })
+  readonly contacto: string;
+
+  @ApiProperty()
+  @IsString({ message: 'El atributo debe ser una cadena de texto' })
+  readonly envio: string;
+
+  @ApiProperty()
+  @IsString({ message: 'El atributo debe ser una cadena de texto' })
+  readonly metodo_envio: string;
+
+  @ApiProperty()
+  @IsNumber({}, { message: 'El atributo debe ser un número' })
+  readonly fecha_venta: number;
+
+  @ApiProperty()
+  @IsNumber({}, { message: 'El atributo debe ser un número' })
+  readonly descuentos: number;
+
+  @ApiProperty()
+  @IsString({ message: 'El atributo debe ser una cadena de texto' })
+  readonly detalle_venta: string;
 }
