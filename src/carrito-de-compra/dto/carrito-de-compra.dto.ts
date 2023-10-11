efff2bd2a56cc75bb571982a99cf6701a5ce6ff7
Alejandra Ayala <onlyourfeeling@hotmail.com>
2023-10-11 17:35:50 -0300

    repetidos

--- src/carrito-de-compra/dto/carrito-de-compra.dto.ts
+++ src/carrito-de-compra/dto/carrito-de-compra.dto.ts
@@ -3,50 +3,50 @@ import { IsString, IsNumber } from 'class-validator';
 
 export class CarritoCompraDto {
   @ApiProperty()
-  @IsNumber({}, { message: 'El atributo debe ser un número' })
+  @IsNumber({}, { message: 'El atributo debe ser un number'})
   readonly venta_id: number;
 
   @ApiProperty()
-  @IsNumber({}, { message: 'El atributo debe ser un número' })
+  @IsNumber({}, { message: 'El atributo debe ser un number' })
   readonly carrito: number;
 
   @ApiProperty()
-  @IsString({ message: 'El atributo debe ser una cadena de texto' })
+  @IsString({ message: 'El atributo debe ser una cadena de string' })
   readonly productos: string;
 
   @ApiProperty()
-  @IsString({ message: 'El atributo debe ser una cadena de texto' })
+  @IsString({ message: 'El atributo debe ser una cadena de string' })
   readonly promociones: string;
 
   @ApiProperty()
-  @IsNumber({}, { message: 'El atributo debe ser un número' })
+  @IsNumber({}, { message: 'El atributo debe ser un number' })
   readonly subtotal: number;
 
   @ApiProperty()
-  @IsNumber({}, { message: 'El atributo debe ser un número' })
+  @IsNumber({}, { message: 'El atributo debe ser un number' })
   readonly total: number;
 
   @ApiProperty()
-  @IsString({ message: 'El atributo debe ser una cadena de texto' })
+  @IsString({ message: 'El atributo debe ser una cadena de string' })
   readonly contacto: string;
 
   @ApiProperty()
-  @IsString({ message: 'El atributo debe ser una cadena de texto' })
+  @IsString({ message: 'El atributo debe ser una cadena de string' })
   readonly envio: string;
 
   @ApiProperty()
-  @IsString({ message: 'El atributo debe ser una cadena de texto' })
+  @IsString({ message: 'El atributo debe ser una cadena de string' })
   readonly metodo_envio: string;
 
   @ApiProperty()
-  @IsNumber({}, { message: 'El atributo debe ser un número' })
+  @IsNumber({}, { message: 'El atributo debe ser un number' })
   readonly fecha_venta: number;
 
   @ApiProperty()
-  @IsNumber({}, { message: 'El atributo debe ser un número' })
+  @IsNumber({}, { message: 'El atributo debe ser un number' })
   readonly descuentos: number;
 
   @ApiProperty()
-  @IsString({ message: 'El atributo debe ser una cadena de texto' })
+  @IsString({ message: 'El atributo debe ser una cadena de string' })
   readonly detalle_venta: string;
 }
