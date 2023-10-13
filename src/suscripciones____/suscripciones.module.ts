8cb6eca05f439a578c707acbaef4a3f86ce579c4
Luis Herrera <l.herrera.garnica@gmail.com>
2023-10-13 20:38:14 -0300

    ⚰️ suscripciones bkp

--- src/suscripciones____/suscripciones.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PacksController } from '../packs/packs.controller';
-import { PacksService } from '../packs/packs.service';
-
-@Module({
-  controllers: [PacksController],
-  providers: [PacksService],
-})
-export class SuscripcionesModule {}
